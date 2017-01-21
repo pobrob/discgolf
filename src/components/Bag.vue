c6b6cfd6ec0263ad401a75d640facc28b832942c
pobrob <rbpollock@brookspollock.com>
2017-01-20 19:40:42 -0500

    turned into panel

--- src/components/Bag.vue
+++ src/components/Bag.vue
@@ -1,14 +1,19 @@
 <template lang="html">
-  <div class="panel panel-default">
-    <div class="panel-body" >
-<slot></slot>
-    <div  v-for="disc in discs">{{disc.disc}}</div>
-  </div>
+  <div class="container">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <slot></slot>
+      </div>
+      <div class="panel-body">
+        <ul class="list-group">
+            <li  v-for="disc in discs" class="list-group-item">{{disc.disc}}</li>
+          </ul>
+      </div>
+    </div>
 </div>
 </template>
 
 <script>
-
 export default {
     props: ['discs']
 }
