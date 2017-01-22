a68a6bc0f6f2263b8a76a76e251b9fe30336a539
pobrob <rbpollock@brookspollock.com>
2017-01-21 21:47:24 -0500

    implemented display

--- src/components/PathViewControl.vue
+++ src/components/PathViewControl.vue
@@ -3,22 +3,32 @@
     <b>Display options:</b><br/>
     <div class="settings toolbox">
         <b>Spread:</b>
-        <input type="checkbox" checked id="fan-power"> &nbsp;
+        <input type="checkbox" v-model="spread" id="fan-power"> &nbsp;
         <b>Paths per disc:</b>
-        <select id="paths-shown"><option value="all">All</option>
-          <option value="one" selected>One</option>
+        <select id="paths-shown" v-model="pathsShown">
+          <option value="all">All</option>
+          <option value="one" >One</option>
           <option value="none">None</option>
         </select> &nbsp; &nbsp;
         <b>Labels:</b>
-        <input type="checkbox" id="lie-distance" checked> &nbsp; <b>Circles (10m/15m):</b>
-        <input type="checkbox" id="lie-circle" checked>
+        <input type="checkbox" id="lie-distance" v-model="lieDistance" > &nbsp; <b>Circles (10m/15m):</b>
+        <input type="checkbox" id="lie-circle" v-model="lieCircle">
         <!-- &nbsp; <b>Debug:</b> <input type="checkbox" id="debug-data"> -->
     </div>
 </div>
 </template>
 
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            pathsShown: "all",
+            spread: true,
+            lieDistance: true,
+            lieCircle: true
+        }
+    }
+}
 </script>
 
 <style lang="css">
